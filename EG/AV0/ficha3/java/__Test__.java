9f9814f824ea67a86a5ab1c48e69e981947adf20
António Silva <anolsi@live.com.pt>
2012-11-16 20:28:17 +0000

    Adição meu __Test__.java para ser enviado também.

--- EG/AV0/ficha3/java/__Test__.java
+++ EG/AV0/ficha3/java/__Test__.java
@@ -6,12 +6,12 @@ import org.antlr.runtime.debug.DebugEventSocketProxy;
 public class __Test__ {
 
     public static void main(String args[]) throws Exception {
-        ficha5Lexer lex = new ficha5Lexer(new ANTLRFileStream("__Test___input.txt", "UTF8"));
+        ficha3Lexer lex = new ficha3Lexer(new ANTLRFileStream("../ficha3.input", "UTF8"));
         CommonTokenStream tokens = new CommonTokenStream(lex);
 
-        ficha5Parser g = new ficha5Parser(tokens, 49101, null);
+        ficha3Parser g = new ficha3Parser(tokens, 49101, null);
         try {
-            g.biblioteca();
+            g.facturas();
         } catch (RecognitionException e) {
             e.printStackTrace();
         }
