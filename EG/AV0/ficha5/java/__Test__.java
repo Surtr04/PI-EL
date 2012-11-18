698bba8e7e6345af13dec124375caf4025ae77d8
Surtr04 <ruibrito666@gmail.com>
2012-11-18 21:15:40 +0000

    Versão final da entrega AV0 já com o zip

---
+++ EG/AV0/ficha5/java/__Test__.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import org.antlr.runtime.*;
+import org.antlr.runtime.debug.DebugEventSocketProxy;
+
+
+public class __Test__ {
+
+    public static void main(String args[]) throws Exception {
+        ficha5Lexer lex = new ficha5Lexer(new ANTLRFileStream("__Test___input.txt", "UTF8"));
+        CommonTokenStream tokens = new CommonTokenStream(lex);
+
+        ficha5Parser g = new ficha5Parser(tokens, 49101, null);
+        try {
+            g.biblioteca();
+        } catch (RecognitionException e) {
+            e.printStackTrace();
+        }
+    }
+}
